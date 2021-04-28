b5c537429b40046237cca318d4cb2a9e31869af0
yuyitian <yuyitian@cn.cogenda.com>
2021-04-28 18:06:46 +0800

    update breath & depth first paths

---
+++ Graph/DepthFirstPaths.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <vector>
+
+#include "Graph.h"
+#include "./Container/Stack.h"
+
+class DepthFirstPaths
+{
+public:
+    DepthFirstPaths(const Graph &G, unsigned s);
+
+    bool hasPathTo(int v) const;
+
+    Stack<int> pathTo(int v) const;
+
+private:
+    void dfs(const Graph &G, unsigned v);
+
+private:
+    std::vector<bool> marked;
+
+    std::vector<int> edgeTo;
+
+    unsigned s; //start point
+};
+
+DepthFirstPaths::DepthFirstPaths(const Graph &G, unsigned s)
+    : marked(G.getV(), false), edgeTo(G.getV(), 0), s{s}
+{
+    dfs(G, s);
+}
+
+void DepthFirstPaths::dfs(const Graph &G, unsigned v)
+{
+    marked[v] = true;
+    for (int w : G.getBag(v))
+        if (!marked[w])
+        {
+            edgeTo[w] = v;
+            dfs(G, w);
+        }
+}
+
+inline bool DepthFirstPaths::hasPathTo(int v) const
+{
+    return marked[v];
+}
+
+Stack<int> DepthFirstPaths::pathTo(int v) const
+{
+
+    if (!hasPathTo(v))
+        return Stack<int>{};
+
+    Stack<int> path{};
+    for (int x = v; x != s; x = edgeTo[x])
+        path.push(x);
+    path.push(s);
+    return path;
+}
